cd707ecb6077bba537c084d83e770508867bb831
krisho27 <krisshreiner@gmail.com>
2024-11-11 15:46:36 -0600

    console test

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
+console.log('test');
+
 // For assistance: 
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
